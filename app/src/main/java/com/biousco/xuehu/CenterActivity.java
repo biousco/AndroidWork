dc831f8a851465d627c3ba71ca9c0973ce3b99dc
Zhangxin <1194271006@qq.com>
2016-06-09 20:50:05 +0800

    zhangxin

---
+++ app/src/main/java/com/biousco/xuehu/CenterActivity.java
@@ -0,0 +1,50 @@
+package com.biousco.xuehu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.annotation.TargetApi;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.Message;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+import com.biousco.xuehu.Cgi.XuehuApi;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+@ContentView(R.layout.activity_user_center)//绑定界面
+public class CenterActivity extends BaseActivity {
+
+}
